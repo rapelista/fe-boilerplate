9afd63b20b3f03adab6d2c723462de0a9b49ac64
Farih Akmal Haqiqi <akmal@gamatecha.com>
2025-03-04 14:23:52 +0700

    refactor: remove ReactQueryDevtools from WithReactQuery component

--- stories/decorators/query.tsx
+++ stories/decorators/query.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { getQueryClient } from '../utils/query';
 
 export interface ProvidersProps extends React.PropsWithChildren {}
@@ -9,10 +8,5 @@ export interface ProvidersProps extends React.PropsWithChildren {}
 export function WithReactQuery({ children }: ProvidersProps) {
   const client = getQueryClient();
 
-  return (
-    <QueryClientProvider client={client}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 }
