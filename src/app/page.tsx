21b5cb306e9895be87d42434a93ef71b91114b4a
Farih Akmal Haqiqi <akmal@gamatecha.com>
2025-03-04 14:10:30 +0700

    feat: add documentation link to the main page and adjust container margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import { Container, Stack, Title } from '@mantine/core';
+import { Container, Stack, Text, Title } from '@mantine/core';
+import Link from 'next/link';
 
 export default function Page() {
   return (
-    <Container my="md">
+    <Container my="xl">
       <Stack>
         <Title>Boilerplate NextJS v15</Title>
+
+        <Text
+          component={Link}
+          href="https://github.com/developergamatecha/frontend-boilerplate?tab=readme-ov-file#boilerplate-v15"
+          c="primary"
+          fw={600}
+          td="underline"
+          style={{ textUnderlineOffset: 2 }}
+        >
+          Read Documentation
+        </Text>
       </Stack>
     </Container>
   );
