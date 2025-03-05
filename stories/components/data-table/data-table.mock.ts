39bd2a489212eebd25f5c5854014b6c288aa3e1a
Farih Akmal Haqiqi <akmal@gamatecha.com>
2025-03-05 14:31:00 +0700

    feat: extend pagination schema to include user age type filtering in data table mock

--- stories/components/data-table/data-table.mock.ts
+++ stories/components/data-table/data-table.mock.ts
@@ -1,7 +1,7 @@
 import { http, HttpResponse } from 'msw';
 import { delay } from '~/utils/core/misc';
 import { USERS } from '../../utils/data';
-import { paginationSchema } from '../../utils/validation';
+import { usersParams } from '../../utils/validation';
 
 const url = 'http://localhost:3000/api/v1/users';
 
@@ -32,20 +32,39 @@ export const DATA_TABLE_MOCK = http.get(url, async ({ request }) => {
   const url = new URL(request.url);
   const params = Object.fromEntries(url.searchParams);
 
-  const { data: validated, error } = paginationSchema.safeParse(params);
+  const { data: validated, error } = usersParams.safeParse(params);
 
   if (error) {
-    return HttpResponse.json({ error: error.errors });
+    return HttpResponse.json(
+      {
+        type: 'client_error',
+        errors: error.errors.map((error) => ({
+          detail: error.message,
+          attr: error.path.join(','),
+          code: error.code,
+        })),
+        timestamp: new Date().toISOString(),
+      },
+      {
+        status: 400,
+      },
+    );
   }
 
-  const { page, limit, search } = validated;
+  const { page, limit, search, type } = validated;
 
   const users = USERS.reverse();
 
-  const filteredData = users.filter((user) => {
-    if (!search) return true;
-    return user.name.toLowerCase().includes(search.toLowerCase());
-  });
+  const filteredData = users
+    .filter((user) => {
+      if (!search) return true;
+      return user.name.toLowerCase().includes(search.toLowerCase());
+    })
+    .filter((user) => {
+      if (type === 'young') return user.age < 30;
+      if (type === 'old') return user.age >= 30;
+      return true;
+    });
 
   const data = filteredData.slice((page - 1) * limit, page * limit);
 
