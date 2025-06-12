b7283662d415cdefbb345f047126aa818d805cfb
Farih Akmal Haqiqi <akmal@gamatecha.com>
2025-06-12 13:37:16 +0700

    remove react-imask

--- src/components/core/phone-number/index.tsx
+++ src/components/core/phone-number/index.tsx
@@ -1,30 +1,18 @@
 'use client';
 
-import {
-  Center,
-  createPolymorphicComponent,
-  TextInput,
-  TextInputProps,
-} from '@mantine/core';
-import { IMaskInput } from 'react-imask';
+import { Center, TextInput, TextInputProps } from '@mantine/core';
 
 interface PhoneNumberInputProps extends TextInputProps {
   withCountryCode?: boolean;
 }
-
-const PolymorphicPhoneNumberInput = createPolymorphicComponent<
-  'input',
-  TextInputProps
->(TextInput);
 
 export function PhoneNumberInput({
   withCountryCode = true,
   ...props
 }: PhoneNumberInputProps) {
   return (
-    <PolymorphicPhoneNumberInput
+    <TextInput
       {...props}
-      component={IMaskInput}
       leftSection={
         withCountryCode ? (
           <Center fz={14} h="100%" px="xs" w="100%">
@@ -34,26 +22,6 @@ export function PhoneNumberInput({
           props.leftSection
         )
       }
-      mask={[
-        {
-          mask: '000-0000-0000', // untuk 12 digit (misal HP panjang)
-        },
-        {
-          mask: '000-000-0000', // untuk 11 digit
-        },
-        {
-          mask: '000-0000-000', // untuk 11 digit dengan variasi lain
-        },
-        {
-          mask: '0000-0000-000', // untuk rumah dengan kode area panjang
-        },
-        {
-          mask: '0000-000-000', // contoh lain
-        },
-        {
-          mask: '000-0000-0000-0', // untuk nomor internasional atau panjang
-        },
-      ]}
       styles={{
         input: {
           minHeight: 40,
